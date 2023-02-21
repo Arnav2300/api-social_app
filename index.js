442663dfe990ba073e32ddb7f8fa4f96b55fc73a
arnavsar <56104491+Arnav2300@users.noreply.github.com>
2023-02-21 16:17:27 +0530

    API completed till get user, delete user, update user

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+const app = express();
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to mongo");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connect();
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.get("/", (req, res) => {
+  res.send("welcome home");
+});
+
+app.listen(8800, () => {
+  console.log("backend server running");
+});
