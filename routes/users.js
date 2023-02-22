da9eb8d993ee24e39b68837947b3cb59592d9ece
arnavsar <56104491+Arnav2300@users.noreply.github.com>
2023-02-22 14:29:15 +0530

    Added follow and unfollow users functionality

--- routes/users.js
+++ routes/users.js
@@ -33,7 +33,8 @@ router.put("/:id", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    res.status(200).json(user);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
   } catch (err) {
     return res.status(500).json(err);
   }
@@ -52,6 +53,45 @@ router.delete("/:id", async (req, res) => {
   }
 });
 //follow a user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { following: req.params.id } });
+        res.status(200).json("user has been followed!");
+      } else {
+        res.status(403).json("already following!");
+      }
+    } catch (err) {
+      res.status(403).json(err);
+    }
+  } else {
+    res.status(403).json("you cannot follow yourself");
+  }
+});
+
 //unfollow a user
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { following: req.params.id } });
+        res.status(200).json("user has been unfollowed!");
+      } else {
+        res.status(403).json("not following!");
+      }
+    } catch (err) {
+      res.status(403).json(err);
+    }
+  } else {
+    res.status(403).json("you cannot follow yourself");
+  }
+});
 
 module.exports = router;
